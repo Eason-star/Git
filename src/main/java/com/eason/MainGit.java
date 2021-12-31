0afe9ba53f87d5cab1c3658febd7b2f3d75dcb8c
Eason-star <E13132281072@163.com>
2021-12-31 16:57:27 +0800

    添加代码

---
+++ src/main/java/com/eason/MainGit.java
@@ -0,0 +1,11 @@
+package com.eason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainGit {
+    public static void main(String[] args) {
+        SpringApplication.run(MainGit.class,args);
+    }
+}
